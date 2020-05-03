412f8494ffd560256b1d66ca26180bd7a4ebf521
Vlad Pisanov <vladimir.pisanov@gmail.com>
2020-05-03 09:01:23 -0400

    Styled dice, added die label formatting

--- app/javascript/components/Board.jsx
+++ app/javascript/components/Board.jsx
@@ -4,25 +4,27 @@
 
 import React           from 'react'
 import { useSelector } from 'react-redux'
-import Chip            from '@material-ui/core/Chip'
 
 export default function Board() {
-  const board = useSelector(state => state.board)
+  const board        = useSelector(state => state.board)
+  const formatLetter = (letter) => letter == 'q' ? 'Qu' : letter.toUpperCase()
   
   return (
     <>
       <h2>Game Board</h2>
       
       <table>
-        {board.map((row, i) => (
-          <tr key={i}>
-            {row.map((letter, j) => (
-              <td key={j}>
-                <Chip label={letter} color="primary" variant="outlined" size="medium" style={{fontFamily: 'monospace'}}/>
-              </td>
-            ))}
-          </tr>
-        ))}
+        <tbody>
+          {board.map((row, i) => (
+            <tr key={i}>
+              {row.map((letter, j) => (
+                <td key={j}>
+                  <div className="die">{formatLetter(letter)}</div>
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
       </table>
     </>
   )
