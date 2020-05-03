be2b3cf5eca4dde613ea1683126f21dbfc9e0bae
Vlad Pisanov <vladimir.pisanov@gmail.com>
2020-05-03 08:05:59 -0400

    Added component for showing global errors

---
+++ app/javascript/components/ErrorPopup.jsx
@@ -0,0 +1,24 @@
+/*
+  A pop-up alert that is displayed whenever the global
+  state's `lastError` is set. Hides automatically in 5s
+*/
+
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import Alert from '@material-ui/lab/Alert'
+import Snackbar from '@material-ui/core/Snackbar'
+import { clearLastError } from '../slices/Master.js'
+
+export default function ErrorPopup() {
+  const lastError = useSelector(state => state.lastError)
+  const dispatch  = useDispatch()
+  const close     = () => dispatch(clearLastError())
+
+  return (
+    <Snackbar open={!!lastError} autoHideDuration={5000} onClose={close}>
+      <Alert onClose={close} severity="error">
+        {lastError}
+      </Alert>
+    </Snackbar>
+  )
+}
