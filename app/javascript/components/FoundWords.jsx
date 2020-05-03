745ffe5790b13039dd718582b033393d69b8344b
Vlad Pisanov <vladimir.pisanov@gmail.com>
2020-05-03 07:51:20 -0400

    Added component for listing found words

---
+++ app/javascript/components/FoundWords.jsx
@@ -0,0 +1,23 @@
+/*
+  A list to display all found words so far, along with their scores
+*/
+
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+export default function FoundWords() {
+  const words = useSelector(state => state.wordsFound)
+
+  return (
+    <>
+      <h2>Found Words</h2>
+      <ul>
+        {words.map(word => (
+          <li key={word}>
+            {word.word} <small>({word.score}pts)</small>
+          </li>
+        ))}
+      </ul>
+    </>
+  )
+}
