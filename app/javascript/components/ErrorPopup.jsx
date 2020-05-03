ed073638cc36a005f8130e577b54da9676ed725c
Vlad Pisanov <vladimir.pisanov@gmail.com>
2020-05-03 08:19:16 -0400

    Cosmetics

--- app/javascript/components/ErrorPopup.jsx
+++ app/javascript/components/ErrorPopup.jsx
@@ -3,11 +3,11 @@
   state's `lastError` is set. Hides automatically in 5s
 */
 
-import React from 'react'
+import React                        from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import Alert from '@material-ui/lab/Alert'
-import Snackbar from '@material-ui/core/Snackbar'
-import { clearLastError } from '../slices/Master.js'
+import Alert                        from '@material-ui/lab/Alert'
+import Snackbar                     from '@material-ui/core/Snackbar'
+import { clearLastError }           from '../slices/Master.js'
 
 export default function ErrorPopup() {
   const error    = useSelector(state => state.lastError)
