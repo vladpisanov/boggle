5ef512791ac14f5513f5ed5eb3f1f9a255b81d23
Vlad Pisanov <vladimir.pisanov@gmail.com>
2020-05-03 15:12:43 -0400

    Added confetti

--- app/javascript/components/FoundWords.jsx
+++ app/javascript/components/FoundWords.jsx
@@ -1,20 +1,36 @@
 /*
-  A list to display all found words so far, along with their scores
+  A list to display all found words so far, along with their scores.
+  Triggers a confetti rain every time a word is added.
 */
 
-import React           from 'react'
-import { useSelector } from 'react-redux'
+import React, { useState, useEffect } from 'react'
+import { useSelector }                from 'react-redux'
+import Confetti                       from 'react-confetti'
 
-export default function FoundWords() {
-  const words = useSelector(state => state.wordsFound)
+export default function FoundWords(props) {
+  const words                 = useSelector(state => state.wordsFound)
+  const [runConf, setRunConf] = useState(false)
+
+  // Watch for changes in words; anytime the list grows, re-trigger the confetti
+  useEffect(() => {
+    if (words.length)
+      setRunConf(true)
+  }, [words])
+
+  const afterConfetti = (confetti) => {
+    setRunConf(false)
+    confetti.reset()
+  }
 
   return (
     <>
+      <Confetti gravity={0.3} initialVelocityY={20} recycle={false} run={runConf} onConfettiComplete={afterConfetti} />
+
       <h2>Found Words</h2>
       <ul>
-        {words.map(word => (
-          <li key={word}>
-            {word.word.toUpperCase()} <small>({word.score}pts)</small>
+        {words.map(rec => (
+          <li key={rec.word}>
+            {rec.word.toUpperCase()} <small>({rec.score}pts)</small>
           </li>
         ))}
       </ul>
